69a104323af4e08fd56d4f76d1ecdfb8ee0f7b3c
junhungYang <393607191@qq.com>
2018-09-13 17:04:16 +0800

    store布置完毕 并且完成provie往store传值

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import router from './router'
 import '../static/reset.css'
 import "../mock/index";
 import ElementUI from 'element-ui'
+import {store} from './store/store'
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
--- src/plugin/proviceSelect.vue
+++ src/plugin/proviceSelect.vue
@@ -11,6 +11,7 @@
 
 <script>
 import axios from 'axios'
+import {mapState,mapMutations} from 'vuex'
   export default {
     data() {
       return {
@@ -19,20 +20,29 @@ import axios from 'axios'
       }
     },
     created() {
-    axios.get('/api/dealer/findListByLv', {
-      lv: 0,
-      pid: 1
-    }).then((res) => {
-      if (res.data.code === 0) {
-        let district = res.data.data
-        district.forEach((item) => {
-          this.options.push({
-            value:item.id,
-            label:item.name
-          })
+        axios.get('/api/dealer/findListByLv', {
+        lv: 0,
+        pid: 1
+        }).then((res) => {
+        if (res.data.code === 0) {
+            let district = res.data.data
+            district.forEach((item) => {
+            this.options.push({
+                value: `${item.id}-${item.name}`,
+                label:item.name
+            })
+            })
+        }
         })
-      }
-    })
-  }
+    },
+    methods: {
+        ...mapMutations(['changeProvince'])
+    },
+    watch: {
+        value() {
+            let arr = this.value.split('-')
+            this.changeProvince(arr)
+        }
+    }
   }
 </script>
